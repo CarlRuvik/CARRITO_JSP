59259c7179539c5cd39c903fb2305fb2df9df9a4
CarlRuvik <carlruvik@gmail.com>
2020-08-08 16:56:57 -0500

    Se agregó la clase que obtiene las datos de usuario que has sido registrados anteriormente de la BD

--- src/java/Servlets/VerificarUsuario.java
+++ src/java/Servlets/VerificarUsuario.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Servlets;
 
 import java.io.IOException;
@@ -12,26 +7,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author navii
- */
 public class VerificarUsuario extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
+            
+            //Primero dbemos obtener los parámetros necesarios desde el formulario(index.html):
+            String srvusername,srvpassword;
+            
+            srvusername=request.getParameter("txtUsuario");
+            srvpassword=request.getParameter("txtPassword");
+            
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
